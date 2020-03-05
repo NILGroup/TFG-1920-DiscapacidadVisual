a20016961d95e867e33b8361846c4d98c63a76ea
bserranoanton <belen523@gmail.com>
2020-03-05 20:55:55 +0100

    CD

---
+++ CD/Cliente/Login/src/com/avanti/camera/Preview.java
@@ -0,0 +1,79 @@
+package com.avanti.camera;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class Preview extends SurfaceView implements SurfaceHolder.Callback {
+    
+	private SurfaceHolder mHolder;
+    private Camera mCamera;
+    public static float _vision_angle;
+    /**
+     * Diámetro de la cámara en mm
+     */
+    @SuppressWarnings("unused")
+	private static double _camera_diameter = 76.2;
+    
+    /**
+     * Distancia focal de la cámara en mm
+     */
+    @SuppressWarnings("unused")
+	private static double _camera_f = 71.12;
+    
+    public Preview(Context context) {
+        super(context);
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, acquire the camera and tell it where
+        // to draw.
+    	try{
+    		
+    		mCamera = Camera.open();   		
+    		if(mCamera!=null){
+	    		//double d = 2 * Math.atan(_camera_diameter/(2*_camera_f));
+	    		//_vision_angle = (float) Math.toDegrees(d);
+	    		_vision_angle = (float) /*50.63;*/50.87;
+		        try {
+		           mCamera.setPreviewDisplay(holder);
+		        } catch (IOException exception) {
+		            mCamera.release();
+		            mCamera = null;
+		        }
+    		}else{
+    			
+    		}
+    	}catch(Exception e){
+    		e.printStackTrace();
+    	}
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Surface will be destroyed when we return, so stop the preview.
+        // Because the CameraDevice object is not a shared resource, it's very
+        // important to release it when the activity is paused.
+        mCamera.stopPreview();        
+        mCamera = null;
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // Now that the size is known, set up the camera parameters and begin
+        // the preview.
+        Camera.Parameters parameters = mCamera.getParameters();
+        parameters.setPreviewSize(w, h);
+        mCamera.setParameters(parameters);
+        mCamera.startPreview();
+    }
+
+    
+}
---
+++ CD/Servidor/CalculaRuta/src/xml/Edificio.java
@@ -0,0 +1,75 @@
+package xml;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import routes.Cuadrante;
+import routes.Estancia;
+
+/**
+ * 
+ * Clase de carga los xml que representan el edificio completo. En el archivo "edificio.xml" se encuentran
+ * los diferentes archivos a cargar.
+ * 
+ * 26/05/2014 - Revisado y limpiado
+ *
+ */
+public class Edificio {
+
+	private CargaXML carga;
+	private ArrayList<Estancia> aEstancias = new ArrayList<Estancia>();
+	private ArrayList<Cuadrante> aCuadrantes= new ArrayList<Cuadrante>();
+
+	public Edificio() {
+
+			 //Se crea un SAXBuilder para poder parsear el archivo
+		    SAXBuilder builder = new SAXBuilder();
+		    File xmlFile = new File( "xml/edificio.xml" );
+		    
+		    try
+		    {
+
+		        Document document = (Document) builder.build( xmlFile );//Se crea el documento a traves del archivo
+		        Element rootNode = document.getRootElement();//Se obtiene la raiz 'edificio'
+		        List lista_plantas = rootNode.getChildren( "planta" );//Se obtiene la lista de hijos de la raiz 'edificio'
+		        
+		        for ( int indexPlanta = 0; indexPlanta < lista_plantas.size(); indexPlanta++ )//Se recorre la lista de 'plantas'
+		        {
+		            Element planta = (Element) lista_plantas.get(indexPlanta);//Se obtiene el elemento 'planta'
+		            String nombre = planta.getAttributeValue("nombre");//Se obtiene el atribullo 'idp'
+		            
+		           
+		           String archivo = planta.getChildText("archivo");
+		           if (!archivo.equals("nada")){
+		        	   carga= new CargaXML(archivo);
+		        	   aEstancias.addAll(carga.getEstancias());
+		        	   aCuadrantes.addAll(carga.getCuadrantes());
+		           }
+		        }
+		    }catch ( IOException io ) {
+		        System.out.println( io.getMessage() );
+		    }catch ( JDOMException jdomex ) {
+		        System.out.println( jdomex.getMessage() );
+		    }
+		}
+	
+	public ArrayList<Estancia> getEstancias(){
+		return aEstancias;
+	}
+	
+	public ArrayList<Cuadrante> getCuadrantes() {
+		return aCuadrantes;
+	}
+	
+	public static void main(String[] args) {
+		new Edificio();
+	}
+
+}
