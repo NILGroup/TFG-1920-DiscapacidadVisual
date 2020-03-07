9d9c466351e8a344e17054c6b2c1c4dacb180e5d
bserranoanton <belen523@gmail.com>
2020-03-07 16:28:18 +0100

    Aquí tengo las prubeas del servidor y el cliente

---
+++ Eclipse_ws/Servidor_Prueba/src/routes/Posicion.java
@@ -0,0 +1,81 @@
+package routes;
+
+/**
+ * 
+ * Clase útil para almacenar los valores de las posiciones de los cuadrantes. Los valores X e Y que se
+ * guarden corresponderán a las coordenadas reales.
+ * 
+ * 26/05/2014 - Revisado y limpiado
+ *
+ */
+public class Posicion {
+	
+	private double pX;
+	private double pY;
+	
+	public Posicion() {
+		
+		pX = 0;
+		pY = 0;
+		
+	}
+	
+	public Posicion(double x, double y) {
+		
+		pX = x;
+		pY = y;
+		
+	}
+	
+	public Posicion(Posicion p) {
+		
+		pX = p.pX;
+		pY = p.pY;
+		
+	}
+	
+	public Posicion clone() {
+		
+		Posicion p = new Posicion();
+		p.setPX(pX);
+		p.setPY(pY);
+		return p;
+		
+	}
+	
+	public boolean equals(Object o) {
+		
+		Posicion p = (Posicion)o;
+		
+		if ((this.pX==p.getPX()) && (this.pY==p.getPY()))
+			return true;
+		else 
+			return false;
+		
+	}
+	
+	public void modificar(double pX, double pY){
+		
+		this.pX = pX;
+		this.pY = pY;
+		
+	}
+	
+	public double getPX(){
+		return pX;
+	}
+	
+	public double getPY(){
+		return pY;
+	}
+	
+	public void setPX(double x){
+		pX = x;
+	}
+	
+	public void setPY(double y){
+		pY = y;
+	}
+
+	
+}
