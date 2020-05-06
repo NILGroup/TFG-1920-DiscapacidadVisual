e27efbff115097514f67b2fa37085f729e10b392
clasuso <55594456+clasuso@users.noreply.github.com>
2020-05-06 20:00:39 +0200

    xml y cambios en appv5

--- app_guia_v5/app/src/main/java/com/example/app_guia_v5/ConfigActivity.java
+++ app_guia_v5/app/src/main/java/com/example/app_guia_v5/ConfigActivity.java
@@ -1,16 +1,13 @@
 package com.example.app_guia_v5;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 
-import android.os.Vibrator;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.Button;
-import androidx.appcompat.app.AlertDialog;
+
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,10 +15,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class ConfigActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private TTSManager ttsManager;
-    MediaPlayer mp;
-    Vibrator vibrator;
 
     public static Intent createIntent(@NonNull Context context) {
         return new Intent(context, ConfigActivity.class);
@@ -32,24 +25,17 @@ public class ConfigActivity extends AppCompatActivity implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Inicializamos el objeto de la clase TTSManager
-        ttsManager = new TTSManager();
-        ttsManager.init(this);
-
-        mp= MediaPlayer.create(this, R.raw.acierto);
-        vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-
         setContentView(R.layout.activity_config);
         setupButtons();
 
     }
 
     private void setupButtons(){
+
         Button volume_button = (Button) findViewById(R.id.volumen_button);
         modo_verb_switch = (Switch) findViewById(R.id.modo_verb_switch);
 
-        //set the initial state of verbose
-        //modo_verb_switch.setChecked(false);
+
 
         volume_button.setOnClickListener(this);
         modo_verb_switch.setOnClickListener(this);
@@ -59,10 +45,7 @@ public class ConfigActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View view) { //switches
         switch (view.getId()) {
             case R.id.volumen_button:
-                onBackPressed();
-                //vibrator.vibrate(1000);
-                //long[] pattern={0,100,500,100,500,100,500,1000};
-                //vibrator.vibrate(pattern,-1);
+
                 break;
             case R.id.modo_verb_switch:
                 ScanningActivity.setVerbose(modo_verb_switch.isChecked());
@@ -73,31 +56,4 @@ public class ConfigActivity extends AppCompatActivity implements View.OnClickLis
         return modo_verb_switch;
     }
 
-    @Override
-    protected void onDestroy() {
-       ttsManager.shutDown();
-        super.onDestroy();
-    }
-    @Override
-    public void onBackPressed() {
-        AlertDialog.Builder popup = new AlertDialog.Builder(this);
-        popup.setMessage("¿Está seguro de que desea finalizar la ruta?");
-        popup.setTitle("Finalizar ruta");
-        popup.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        finish();
-                    }
-                });
-                popup.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-
-        AlertDialog dialog = popup.create();
-        dialog.show();
-        //super.onBackPressed();
-    }
 }
